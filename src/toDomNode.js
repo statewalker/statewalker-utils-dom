8edf25d3fecaa3dc4091ae2fab341c896e95a2fa
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2022-10-11 14:03:34 +0200

    refactor: check Node instances

--- src/toDomNode.js
+++ src/toDomNode.js
@@ -3,11 +3,7 @@ export default function toDomNode(value, doc = document) {
   if (value === null) value = "";
   if (typeof value === "number" || typeof value === "boolean")
     value = String(value);
-  if (
-    typeof value === "object" &&
-    !(value instanceof Element || value instanceof Text) &&
-    !(value instanceof value.constructor)
-  ) {
+  if (typeof value === "object" && !(value instanceof Node)) {
     value = Object.prototype.toString.call(value);
   }
   if (typeof value === "string") value = doc.createTextNode(value);
