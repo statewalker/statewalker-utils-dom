2816235015323a377e89e5ddac28f2866bb10600
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2022-10-17 13:26:37 +0200

    refactor: convert arrays to dom nodes

--- src/toDomNode.js
+++ src/toDomNode.js
@@ -1,11 +1,20 @@
-export default function toDomNode(value, doc = document) {
+export default function toDomNode(value, doc = document, containerElm = "span") {
   if (value === undefined) return;
   if (value === null) value = "";
   if (typeof value === "number" || typeof value === "boolean")
     value = String(value);
   if (typeof value === "object" && !(value instanceof Node)) {
-    value = Object.prototype.toString.call(value);
+    if (value !== null && value[Symbol.iterator]) {
+      const elm = doc.createElement(containerElm);
+      for (let val of value) {
+        elm.appendChild(toDomNode(val, doc));
+      }
+      value = elm;
+    } else {
+      value = Object.prototype.toString.call(value);
+    }
   }
   if (typeof value === "string") value = doc.createTextNode(value);
   return value;
 }
+
